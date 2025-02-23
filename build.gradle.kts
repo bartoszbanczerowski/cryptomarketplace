d298c52c85aed7e9a59b46131b54af66ca992774
bartoszbanczerowski <bartosz.banczerowski@circlekeurope.com>
2025-02-23 08:40:12 +0100

    [Task-7] Add firebase bom and update libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
 }
 
 buildscript {
